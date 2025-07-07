6ef033ccda0a4b38e71ea855ee1ac62ec1c45e57
pramadito <pramadito@gmail.com>
2025-07-07 09:16:28 +0700

    grid cols update for mobile

--- src/app/blogs/_components/BlogList.tsx
+++ src/app/blogs/_components/BlogList.tsx
@@ -8,7 +8,7 @@ const BlogList = () => {
   const { data: blogs, isPending } = useGetBlogs();
 
   return (
-    <section className="grid grid-cols-3 gap-8">
+    <section className="grid md:grid-cols-3 grid-cols-1 gap-8">
       {isPending && <BlogCardSkeleton />}
 
       {blogs?.map((blog, index) => (
