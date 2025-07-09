71f85334b9559078828ed818be565520b4986e46
pramadito <pramadito@gmail.com>
2025-07-09 22:24:27 +0700

    update image from png to webp

--- src/components/Navbar/logo.tsx
+++ src/components/Navbar/logo.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export const Logo = () => (
   <Link href="/">
     <div className="flex items-center gap-2">
-      <img src="/garuda.png" className="h-16 w-auto"></img>
+      <img src="/garuda.webp" className="h-16 w-auto"></img>
       <h1 className="text-xs dark:text-white font-bold">
         KEDUTAAN BESAR <br />
         REPUBLIK INDONESIA
